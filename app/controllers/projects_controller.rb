f556d23899e5c7316d07234760bd4766ae747816
Sergio Montoya <sergio.montoya.c@gmail.com>
2018-08-08 15:34:01 -0500

    First test pass and project creation works fine

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,29 @@ class ProjectsController < ApplicationController
     
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      flash[:notice] = "The project was successfully created!"
+      redirect_to @project
+    else
+      
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -4,8 +4,8 @@ RSpec.feature 'User can create new project' do
   scenario 'with valida attributes' do
     visit '/'
     click_link 'New Project'
-    fill_in 'Name', with: 'React JS'
-    fill_in 'Description', with: 'An awesome FrontEnd Framework'
+    fill_in 'project[name]', with: 'React JS'
+    fill_in 'project[description]', with: 'An awesome FrontEnd Framework'
     click_button 'Create Project'
     expect(page).to have_content "The project was successfully created!"
   end
